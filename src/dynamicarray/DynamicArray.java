feb6ba4036f409f2b128e33968140ab82373df05
MSA <MSA@DESKTOP-M8T3NLP.local>
2020-11-05 14:30:12 +0300

    metodu ekledim

--- src/dynamicarray/DynamicArray.java
+++ src/dynamicarray/DynamicArray.java
@@ -1,22 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dynamicarray;
 
-/**
- *
- * @author MSA
- */
 public class DynamicArray {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private int[] array = null;
+    private int kapasite = 5;
+    private int boyut = 0;
+    private int index = 0;
+
+    public DynamicArray() {
+        array = new int[kapasite];
     }
     
+    public void ekle(int veri) {
+        
+        if (boyut==kapasite) {
+            dizi_genislet();
+        }
+        
+        array[boyut]=veri;
+        boyut++;
+    }
+
+    public void ekle(int veri, int index) {
+        
+        if (boyut==kapasite) {
+            dizi_genislet();
+        }
+        
+        for (int i = boyut; i > index; i--) {
+            array[i]=array[i-1];
+        }
+        
+        array[index]=veri;
+        boyut++;
+    }
+    
+    public void dizi_genislet(){
+        //dizinin boyutunu iki katına çıkarır
+        int[] newArray=new int[kapasite*2];
+        
+        //array dizisindeki elemanları newArray e atar
+        for (int i = 0; i < kapasite; i++) {
+            newArray[i]=array[i];                    
+        }
+        
+        array=newArray;
+        
+        this.kapasite=kapasite*2;
+    }
+    
+    public void sil() {
+
+    }
+
+    public void sil(int veri) {
+        array[boyut]=veri;
+        boyut--;
+    }
+
+    public void sil_index(int index){
+        
+        
+    }
+    
+    public int kapasite(){
+        return this.kapasite;        
+    }
+    
+    public void temizle(){
+        array=new int[kapasite];        
+    }
+    
+    public int uzunluk(){
+        return this.boyut;    
+    }
+    
+    
+    public static void main(String[] args) {
+
+    }
+
 }
+
